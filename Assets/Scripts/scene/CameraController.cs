bff0bced02b792cd6c821b5f49e8bbfc10da3acd
Eliot Lash <fadookie@gmail.com>
2021-01-29 19:02:18 -0800

    sidescroller

---
+++ Assets/Scripts/scene/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float Speed = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        var newPos = transform.position;
+        newPos.x += Speed * Time.deltaTime;
+        transform.position = newPos;
+    }
+}
