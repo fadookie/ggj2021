3c058324dc37ac87fe7e7b3a786152f565b7bfcd
Eliot Lash <fadookie@gmail.com>
2021-01-30 20:11:17 -0800

    add player death condition, animation, retry screen, sfx player and hit SFX

---
+++ Assets/Scripts/music/SfxPlayer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using com.eliotlash.core.service;
+using UniRx;
+using UnityEngine;
+
+public class SfxPlayer : MonoBehaviour
+{
+    public AudioClip DamageSound;
+
+    private AudioSource player;
+
+    public enum Sound
+    {
+        Damage,
+    }
+
+    void Awake() {
+        Services.instance.Set(this);
+        player = GetComponent<AudioSource>();
+    }
+
+    void Start() {
+    }
+
+    public void PlaySound(Sound sound) {
+        Debug.LogWarning($"PlaySound: {sound}");
+        switch (sound) {
+            case Sound.Damage:
+                PlayClip(DamageSound);
+                break;
+            default:
+                throw new ArgumentException($"Unknown sound type: {sound}");
+        }
+    }
+
+    private void PlayClip(AudioClip clip) {
+        player.PlayOneShot(clip);
+    }
+}
